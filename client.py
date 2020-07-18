2b71442f3f3827f88faca812cb3df475513b044e
unkonown <amachi057@gmail.com>
2020-07-18 22:56:41 +0900

    a

--- client.py
+++ client.py
@@ -1,13 +1,14 @@
 import requests
-PREURL='https://apitest010.herokuapp.com/'
-#this URL is web api URL/<KEY>
+from jtalk import jtalk
+PREURL = 'https://apitest010.herokuapp.com/'
+# this URL is web api URL/<KEY>
 if __name__ == "__main__":
     while True:
-        text=input('> ')
+        text = input('> ')
         if not text:
             break
-        url=PREURL+text
-        response=requests.get(url)
+        url = PREURL+text
+        response = requests.get(url)
         json_data = response.json()
-        print(json_data['response'])
-
+        print(json_data)
+        jtalk(json_data['response'])
---
+++ jtalk.py
@@ -0,0 +1,17 @@
+#coding: utf-8
+import subprocess
+
+
+def jtalk(t):
+    open_jtalk = ['open_jtalk']
+    mech = ['-x', '/var/lib/mecab/dic/open-jtalk/naist-jdic']
+    htsvoice = ['-m', 'voice/mei_normal.htsvoice']
+    speed = ['-r', '1.0']
+    outwav = ['-ow', 'open_jtalk.wav']
+    cmd = open_jtalk+mech+htsvoice+speed+outwav
+    c = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+    c.stdin.write(t.encode())
+    c.stdin.close()
+    c.wait()
+    aplay = ['aplay', '-q', 'open_jtalk.wav']
+    wr = subprocess.Popen(aplay)
